a468d522c8ae97fc7fccce70b866026121d8c2c8
Narisipuram-Jagadish <nariripuramjkumar@gmail.com>
2021-07-25 16:54:54 +0530

    master updated some code

--- src/test/java/org/dummy/Dummy.java
+++ src/test/java/org/dummy/Dummy.java
@@ -5,6 +5,13 @@ public class Dummy {
 	public static void main(String[] args) {
 		System.out.println("GIT");
 		System.out.println("2nd team member changed some code");
+		//Master is updating 
+		System.out.println("After susccessfully merging master copy , now master is updating new code");
+		int a=10;
+		int  b= 20;
+		a=a+b;
+		a=a-b;
+		b=a-b;
 	}
 
 }
